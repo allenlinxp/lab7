5eb2126d9f40dcdb63b75e02e43979fcde6997d6
allenlinxp <allenlinxp@hotmail.com>
2018-02-22 01:25:11 -0800

    added

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,9 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+  $(".likeCtr").click(function(e) {
+    console.log("YES!!!!!!!!!!!");
+    ga('send', 'event', 'like', 'click');
+  });
 }
+
